ec61bebec410427d7fc842d3269d292b573d4d00
Borteq2 <borteq2@yandex.ru>
2023-03-03 17:02:29 +0300

    sticker id giving

--- lesson1/main.py
+++ lesson1/main.py
@@ -30,16 +30,22 @@ async def start_command(message: types.Message):
     await message.answer('<em>скучное приветствие</em>', parse_mode='html')
 
 
+@dp.message_handler(content_types=['sticker'])
+async def get_sticker_id(message: types.Message):
+    await message.answer(message.sticker.file_id)
+
+
 @dp.message_handler(commands=['give'])
 async def give_command(message: types.Message):
     # @idstickerbot
-    await bot.send_sticker(message.from_user.id, sticker='CAACAgIAAxkBAAEH-YtkAe_WYGManU_Vc5TDLUOpQpKw7wACpSAAArnlcEskT9sMLTkXCC4E')
+    # await bot.send_sticker(message.from_user.id, sticker='CAACAgIAAxkBAAEH-YtkAe_WYGManU_Vc5TDLUOpQpKw7wACpSAAArnlcEskT9sMLTkXCC4E')
+    await bot.send_sticker(message.from_user.id, sticker='CAACAgIAAxkBAAOFZAH8nkhd1UDiBLyYqabDLIe_jg0AAjIAAwXOwi1jNg8IvD8ecC4E')
     await message.delete()
 
-
-@dp.message_handler()
-async def give_emoji(message: types.Message):
-    await message.reply(f'{message.text}' ' 🥳')
+#
+# @dp.message_handler()
+# async def give_emoji(message: types.Message):
+#     await message.reply(f'{message.text}' ' 🥳')
 
 # @dp.message_handler(commands=['description'])
 # async def description_command(message: types.Message):
