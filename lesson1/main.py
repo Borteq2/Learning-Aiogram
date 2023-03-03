cb57abb14a22cdb1d4f51a848df844a5cc358f86
Borteq2 <borteq2@yandex.ru>
2023-03-03 17:25:31 +0300

    administrative methods + send photo + location

--- lesson1/main.py
+++ lesson1/main.py
@@ -9,11 +9,11 @@ load_dotenv()
 
 TOKEN_API = os.environ.get('BOT_TOKEN')
 
-HELP_COMMAND = \
-    """
-    /help - список команд
-/start - начать работу с ботом
-    """
+HELP_COMMAND = """
+<b>/help</b> - <em>список команд</em>
+<b>/start</b> - <em>начать работу с ботом</em>
+<b>/картинка</b> - <em>пиписька</em>
+"""
 
 bot = Bot(TOKEN_API)
 dp = Dispatcher(bot)
@@ -24,23 +24,46 @@ count = 0
 async def on_startup(_):
     print('Я РОДИВСЯ')
 
-
-@dp.message_handler(commands=['start'])
+# для групп
+@dp.message_handler(commands=['help'])
 async def start_command(message: types.Message):
-    await message.answer('<em>скучное приветствие</em>', parse_mode='html')
-
-
-@dp.message_handler(content_types=['sticker'])
-async def get_sticker_id(message: types.Message):
-    await message.answer(message.sticker.file_id)
-
-
-@dp.message_handler(commands=['give'])
-async def give_command(message: types.Message):
-    # @idstickerbot
-    # await bot.send_sticker(message.from_user.id, sticker='CAACAgIAAxkBAAEH-YtkAe_WYGManU_Vc5TDLUOpQpKw7wACpSAAArnlcEskT9sMLTkXCC4E')
-    await bot.send_sticker(message.from_user.id, sticker='CAACAgIAAxkBAAOFZAH8nkhd1UDiBLyYqabDLIe_jg0AAjIAAwXOwi1jNg8IvD8ecC4E')
+    await bot.send_message(chat_id=message.from_user.id,
+                           text=HELP_COMMAND,
+                           parse_mode='html')
     await message.delete()
+
+
+@dp.message_handler(commands=['картинка'])
+async def send_image(message: types.Message):
+    await bot.send_photo(chat_id=message.from_user.id,
+                        photo='https://2ch.hk/b/src/266160769/16492546145510.png')
+    await message.delete()
+
+
+@dp.message_handler(commands=['location'])
+async def send_point(message: types.Message):
+    await bot.send_location(chat_id=message.from_user.id,
+                            latitude=55,
+                            longitude=74)
+    await message.delete()
+
+
+# @dp.message_handler(commands=['start'])
+# async def start_command(message: types.Message):
+#     await message.answer('<em>скучное приветствие</em>', parse_mode='html')
+#
+#
+# @dp.message_handler(content_types=['sticker'])
+# async def get_sticker_id(message: types.Message):
+#     await message.answer(message.sticker.file_id)
+#
+#
+# @dp.message_handler(commands=['give'])
+# async def give_command(message: types.Message):
+#     # @idstickerbot
+#     # await bot.send_sticker(message.from_user.id, sticker='CAACAgIAAxkBAAEH-YtkAe_WYGManU_Vc5TDLUOpQpKw7wACpSAAArnlcEskT9sMLTkXCC4E')
+#     await bot.send_sticker(message.from_user.id, sticker='CAACAgIAAxkBAAOFZAH8nkhd1UDiBLyYqabDLIe_jg0AAjIAAwXOwi1jNg8IvD8ecC4E')
+#     await message.delete()
 
 #
 # @dp.message_handler()
@@ -86,5 +109,5 @@ async def give_command(message: types.Message):
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp, on_startup=on_startup)
+    executor.start_polling(dp, on_startup=on_startup, skip_updates=True)
 
